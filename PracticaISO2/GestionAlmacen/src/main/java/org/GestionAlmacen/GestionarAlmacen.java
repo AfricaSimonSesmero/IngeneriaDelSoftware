affa35919d4c2a44d462f90b8234212eb43323a3
SaraSevillano <55829207+SaraSevillano@users.noreply.github.com>
2019-11-15 13:47:12 +0100

    Add files via upload

---
+++ PracticaISO2/GestionAlmacen/src/main/java/org/GestionAlmacen/GestionarAlmacen.java
@@ -0,0 +1,25 @@
+package org.GestionAlmacen;
+
+public class GestionarAlmacen {
+
+	public boolean AprovisionarAlmacen() {
+		// TODO - implement GestionarAlmacen.AprovisionarAlmacen
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean ActualizarPiezasStock() {
+		// TODO - implement GestionarAlmacen.ActualizarPiezasStock
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean ControlPiezas() {
+		// TODO - implement GestionarAlmacen.ControlPiezas
+		throw new UnsupportedOperationException();
+	}
+
+	public GestionarAlmacen() {
+		// TODO - implement GestionarAlmacen.GestionarAlmacen
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PracticaISO2/GestionAlmacen/src/main/java/org/GestionAlmacen/Pieza.java
@@ -0,0 +1,64 @@
+package org.GestionAlmacen;
+
+public class Pieza {
+
+	private int IDPieza;
+	private String NombrePieza;
+	private String TipoPieza;
+	private int PrecioPieza;
+	private int CantidadPiezas;
+
+	public int getIDPieza() {
+		return this.IDPieza;
+	}
+
+	public void setIDPieza(int IDPieza) {
+		this.IDPieza = IDPieza;
+	}
+
+	public String getNombrePieza() {
+		return this.NombrePieza;
+	}
+
+	public void setNombrePieza(String NombrePieza) {
+		this.NombrePieza = NombrePieza;
+	}
+
+	public String getTipoPieza() {
+		return this.TipoPieza;
+	}
+
+	public void setTipoPieza(String TipoPieza) {
+		this.TipoPieza = TipoPieza;
+	}
+
+	public int getPrecioPieza() {
+		return this.PrecioPieza;
+	}
+
+	public void setPrecioPieza(int PrecioPieza) {
+		this.PrecioPieza = PrecioPieza;
+	}
+
+	public int getCantidadPiezas() {
+		return this.CantidadPiezas;
+	}
+
+	public void setCantidadPiezas(int CantidadPiezas) {
+		this.CantidadPiezas = CantidadPiezas;
+	}
+
+	/**
+	 * 
+	 * @param IDPieza
+	 * @param NombrePieza
+	 * @param TipoPieza
+	 * @param PrecioPieza
+	 * @param CantidadPiezas
+	 */
+	public Pieza(int IDPieza, String NombrePieza, String TipoPieza, int PrecioPieza, int CantidadPiezas) {
+		// TODO - implement Pieza.Pieza
+		throw new UnsupportedOperationException();
+	}
+
+}
