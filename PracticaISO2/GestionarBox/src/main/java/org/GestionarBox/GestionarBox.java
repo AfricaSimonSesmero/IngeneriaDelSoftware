adcb24e7faf7f284dc567fd80ad63717e055c16b
irr97 <56109061+irr97@users.noreply.github.com>
2019-11-20 19:13:11 +0100

    Add files via upload

---
+++ PracticaISO2/GestionarBox/src/main/java/org/GestionarBox/GestionarBox.java
@@ -0,0 +1,25 @@
+package org.GestionarBox;
+
+public class GestionarBox {
+
+	public void ActualizarEstado() {
+		// TODO - implement GestionarBox.ActualizarEstado
+		throw new UnsupportedOperationException();
+	}
+
+	public void AgregarEstado() {
+		// TODO - implement GestionarBox.AgregarEstado
+		throw new UnsupportedOperationException();
+	}
+
+	public void EliminarEstado() {
+		// TODO - implement GestionarBox.EliminarEstado
+		throw new UnsupportedOperationException();
+	}
+
+	public void AsignarTurno() {
+		// TODO - implement GestionarBox.AsignarTurno
+		throw new UnsupportedOperationException();
+	}
+
+}
