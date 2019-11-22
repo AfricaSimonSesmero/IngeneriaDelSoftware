c781149a518808157da68bf8309fd4796cca0228
AfricaSimonSesmero <55829240+AfricaSimonSesmero@users.noreply.github.com>
2019-11-22 10:30:07 +0100

    Add files via upload

---
+++ PracticaISO2/GestionarListaPiezas/src/main/java/org/GestionarListaPiezas/Pieza.java
@@ -0,0 +1,126 @@
+package org.GestionarListaPiezas;
+
+public class Pieza {
+
+	private int IDPieza;
+	private String NombrePieza;
+	private String TipoPieza;
+	private int PrecioPieza;
+	private int CantidadPiezas;
+	private int CodigoPieza;
+	private boolean Disponibilidad;
+
+	/**
+	 * 
+	 * @param IDPieza
+	 * @param NombrePieza
+	 * @param TipoPieza
+	 * @param PrecioPieza
+	 * @param CantidadPiezas
+	 * @param CodigoPieza
+	 * @param Disponibilidad
+	 */
+	public Pieza(int IDPieza, String NombrePieza, String TipoPieza, int PrecioPieza, int CantidadPiezas, int CodigoPieza, boolean Disponibilidad) {
+		// TODO - implement Pieza.Pieza
+		throw new UnsupportedOperationException();
+	}
+
+	public int getIDPieza() {
+		// TODO - implement Pieza.getIDPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param IDPieza
+	 */
+	public void setIDPieza(int IDPieza) {
+		// TODO - implement Pieza.setIDPieza
+		throw new UnsupportedOperationException();
+	}
+
+	public String getNombrePieza() {
+		// TODO - implement Pieza.getNombrePieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param NombrePieza
+	 */
+	public void setNombrePieza(String NombrePieza) {
+		// TODO - implement Pieza.setNombrePieza
+		throw new UnsupportedOperationException();
+	}
+
+	public String getTipoPieza() {
+		// TODO - implement Pieza.getTipoPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param TipoPieza
+	 */
+	public void setTipoPieza(String TipoPieza) {
+		// TODO - implement Pieza.setTipoPieza
+		throw new UnsupportedOperationException();
+	}
+
+	public int getPrecioPieza() {
+		// TODO - implement Pieza.getPrecioPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param PrecioPieza
+	 */
+	public void setPrecioPieza(int PrecioPieza) {
+		// TODO - implement Pieza.setPrecioPieza
+		throw new UnsupportedOperationException();
+	}
+
+	public int getCantidadPiezas() {
+		// TODO - implement Pieza.getCantidadPiezas
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param CantidadPiezas
+	 */
+	public void setCantidadPiezas(int CantidadPiezas) {
+		// TODO - implement Pieza.setCantidadPiezas
+		throw new UnsupportedOperationException();
+	}
+
+	public int getCodigoPieza() {
+		// TODO - implement Pieza.getCodigoPieza
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param CodigoPieza
+	 */
+	public void setCodigoPieza(int CodigoPieza) {
+		// TODO - implement Pieza.setCodigoPieza
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean getDisponibilidad() {
+		// TODO - implement Pieza.getDisponibilidad
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Disponibilidad
+	 */
+	public void setDisponibilidad(boolean Disponibilidad) {
+		// TODO - implement Pieza.setDisponibilidad
+		throw new UnsupportedOperationException();
+	}
+
+}
