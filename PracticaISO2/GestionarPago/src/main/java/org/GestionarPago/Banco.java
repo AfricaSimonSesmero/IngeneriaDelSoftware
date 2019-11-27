83b36d1d58809f7f8b06818d88d961f00727579e
JavierPalomaresGarrido <56022626+JavierPalomaresGarrido@users.noreply.github.com>
2019-11-27 17:14:11 +0100

    Add files via upload

---
+++ PracticaISO2/GestionarPago/src/main/java/org/GestionarPago/Banco.java
@@ -0,0 +1,36 @@
+package org.GestionarPago;
+
+public class Banco {
+
+	private int Sucursal;
+	private String Nombre;
+
+	public void getSucursal() {
+		// TODO - implement Banco.getSucursal
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Sucursal
+	 */
+	public void setSucursal(int Sucursal) {
+		// TODO - implement Banco.setSucursal
+		throw new UnsupportedOperationException();
+	}
+
+	public String getNombre() {
+		// TODO - implement Banco.getNombre
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Nombre
+	 */
+	public void setNombre(String Nombre) {
+		// TODO - implement Banco.setNombre
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PracticaISO2/GestionarPago/src/main/java/org/GestionarPago/Fecha.java
@@ -0,0 +1,78 @@
+package org.GestionarPago;
+
+public abstract class Fecha {
+
+	private int Dia;
+	private int Mes;
+	private int Anio;
+	private int Hora;
+
+	/**
+	 * 
+	 * @param Dia
+	 * @param Mes
+	 * @param A�o
+	 * @param Hora
+	 */
+	public void Reserva(int Dia, int Mes, int Anio, int Hora) {
+		// TODO - implement Fecha.Reserva
+		throw new UnsupportedOperationException();
+	}
+
+	public int getDia() {
+		// TODO - implement Fecha.getDia
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Dia
+	 */
+	public void setDia(int Dia) {
+		// TODO - implement Fecha.setDia
+		throw new UnsupportedOperationException();
+	}
+
+	public int getMes() {
+		// TODO - implement Fecha.getMes
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Mes
+	 */
+	public void setMes(int Mes) {
+		// TODO - implement Fecha.setMes
+		throw new UnsupportedOperationException();
+	}
+
+	public int getAnio() {
+		// TODO - implement Fecha.getA�o
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param A�o
+	 */
+	public void setAnio(int Anio) {
+		// TODO - implement Fecha.setA�o
+		throw new UnsupportedOperationException();
+	}
+
+	public int getHora() {
+		// TODO - implement Fecha.getHora
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Hora
+	 */
+	public void setHora(int Hora) {
+		// TODO - implement Fecha.setHora
+		throw new UnsupportedOperationException();
+	}
+
+}
