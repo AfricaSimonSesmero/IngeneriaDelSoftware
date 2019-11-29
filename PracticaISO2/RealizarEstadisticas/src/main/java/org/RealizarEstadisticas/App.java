71097b96203683cfa7b88c152f63c22e655ad7db
irr97 <56109061+irr97@users.noreply.github.com>
2019-11-29 13:36:44 +0100

    Add files via upload

---
+++ PracticaISO2/RealizarEstadisticas/src/main/java/org/RealizarEstadisticas/App.java
@@ -0,0 +1,13 @@
+package org.RealizarEstadisticas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ PracticaISO2/RealizarEstadisticas/src/main/java/org/RealizarEstadisticas/RealizarEstadisticas.java
@@ -0,0 +1,24 @@
+package org.RealizarEstadisticas;
+public class RealizarEstadisticas {
+
+	public boolean CalcularMedioEspera() {
+		// TODO - implement RealizarEstad�sticas.CalcularMedioEspera
+		throw new UnsupportedOperationException();
+	}
+
+	public boolean CalcularMedioGeneracioCuenta() {
+		// TODO - implement RealizarEstad�sticas.CalcularMedioGeneracioCuenta
+		throw new UnsupportedOperationException();
+	}
+
+	public int CalcularMedioPreparacionBox() {
+		// TODO - implement RealizarEstad�sticas.CalcularMedioPreparacionBox
+		throw new UnsupportedOperationException();
+	}
+
+	public int CalcularMedioReparacion() {
+		// TODO - implement RealizarEstad�sticas.CalcularMedioReparacion
+		throw new UnsupportedOperationException();
+	}
+
+}
